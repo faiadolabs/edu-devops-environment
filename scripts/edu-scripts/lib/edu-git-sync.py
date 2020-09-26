e7f65e3d3d838b1c788c855b0527101d0ce1a08a
Enrique Agrasar <enrique.agrasar@edu.xunta.es>
2020-09-26 14:02:42 +0200

    Mejoras en edu-git-sync

--- scripts/edu-scripts/lib/edu-git-sync.py
+++ scripts/edu-scripts/lib/edu-git-sync.py
@@ -14,34 +14,34 @@
     \033[1m repos \033[0m: 
         Sintaxis: edu-git-sync <path> repos
         Descripción: Dado un <path> localiza recursivamente todos los repositorios y los muestra en pantaña 
-        distinguiendo los BARE de los que tienes workings directory.
+        distinguiendo los BARE de los que tienen working directories.
         Parámetros: Sin paránetros.
 
     \033[1m dirty \033[0m: 
         Sintaxis: edu-git-sync <path> dirty
-        Descripción: Dado un <path> localiza recursivamente todos los repositorios no BARE que tienes espacios de
-        de trabajo sucio (commits pendientes). 
+        Descripción: Dado un <path> localiza recursivamente todos los repositorios no BARE que tienen espacios de
+        de trabajo sucios (commits pendientes). 
         Parámetros: Sin paránetros.
 
     \033[1m connect \033[0m: 
         Sintaxis: edu-git-sync <path> connect <parámetros>
-        Descripción: Dado un <path> localiza recursivamente todos los repositorios no BARE y los conecta a un 
-        repositorio remoto, con una url especificada por ssh.
+        Descripción: Dado un <path> localiza recursivamente todos los repositorios y los conecta a un  repositorio 
+        remoto, con una url especificada por ssh.
         Parámetros: 
             --host <IP|name>
             --name <remote_rame>
 
     \033[1m fetch \033[0m: 
         Sintaxis: edu-git-sync <path> fetch <parámetros>
-        Descripción: Dado un <path> localiza recursivamente todos los repositorios no BARE y realiza un fetch de todas 
-        las referencias remotas o sólo aquella especificada por parámetro
+        Descripción: Dado un <path> localiza recursivamente todos los repositorios y realiza un fetch de todas las
+        referencias remotas o sólo aquella especificada por parámetro
         Parámetros: 
-            [--remote <remote_name>]
+            [--name <remote_name>]
 
 """
 
 import sys
-from os import walk, listdir, getlogin
+from os import walk, listdir, getlogin, uname
 from os.path import isfile, isdir, join, abspath
 
 # TODO Aviso en caso de no tener las dependencias instaladas: (pip install gitpython)
@@ -103,47 +103,62 @@ def __search_repos__(repos, path):
             # Fin recursión
             pass
 
+
+def tipo(repo):
+    return bcolors.warning("[BARE]") if repo.bare else ""
+
 def __print_repos__(repos):
     print(bcolors.HEADER + "\nRepositorios:\n" + bcolors.ENDC)
     for repo in repos:
-        if not repo.bare:
-            print(bcolors.info("[REPO]: "), repo)
-        else: 
-            print(bcolors.warning("[BARE]: "), repo)
+        print("{} {} {}".format(bcolors.info("[REPO]")), tipo(repo), repo)
     print(bcolors.BOLD + "\nTotal de Respos: ", len(repos), "\n\n" + bcolors.ENDC)
         
 
-def __create_remote__(repo, host, path):
+def __create_remote__(repo, name, path):
     for un_remote in repo.remotes:
-        if un_remote.name == host:
-            print(bcolors.warning("[WARN]: "), "Ya existe el remoto {} para {}".format(host, repo))
+        if un_remote.name == name:
+            print(bcolors.warning("[WARN]: "), "Ya existe el remoto {} para {}".format(name, repo))
             return None
 
     # Procediendo con el nuevo remoto y fetch
-    print("CREANDO REMOTO PRO PARA: ", repo)
-    return repo.create_remote(host, path)
+    tipo = bcolors.warning("[BARE]") if repo.bare else ""
+    print(bcolors.ok("[OK]: "), "Creado el remoto {} {} para {}".format(tipo, name, repo))
+    return repo.create_remote(name, path)
 
 def __fetch_repos__(*repos, name_remote=None):
+    print("\n", bcolors.header("Fetch Repositorios:"), "\n")
+    num_fetched = 0
     for repo in repos:
-        if not repo.bare:
-            print("\n" + bcolors.bold("[FECHING...] "), repo.working_tree_dir)
-            remotes = repo.remotes if name_remote is None else [repo.remote(name=name_remote)]
-            for un_remote in remotes:
-                try:
-                    # TODO: Necesario establecer un timeout: https://stackoverflow.com/questions/492519/timeout-on-a-function-call
-                    fetch_info = un_remote.fetch()
-                    print("\t", bcolors.ok("[Ok fetched]"), un_remote)
-                except Exception as e:
-                    print("\t", bcolors.error("[ERROR fetching]"), un_remote)
+        print("\n" + bcolors.bold("[FECHING...]"), tipo(repo), repo.git_dir)
+        remotes = repo.remotes if name_remote is None else [repo.remote(name=name_remote)]
+        for un_remote in remotes:
+            try:
+                # TODO: Necesario establecer un timeout: https://stackoverflow.com/questions/492519/timeout-on-a-function-call
+                fetch_info = un_remote.fetch()
+                print("\t", bcolors.ok("[Ok fetched]"), un_remote)
+                num_fetched += 1
+            except Exception as e:
+                if e.status == 128 :
+                    print("\t", bcolors.error("[ERROR fetching]"), un_remote, "does not appear to be a git repository")
+                    # Sugerir cómo clonarlo
+                    local_host = uname()[1]
+                    name_host = local_host.split(".")[0].lower()
+                    local_user = str(getlogin())
+                    git_dir = repo.git_dir if repo.bare else repo.working_tree_dir
+                    print("\t Clone on remote:", bcolors.info("git clone -o {} ssh://enrique@{}:{}".format(name_host, local_host, git_dir)))
+                else:
+                    print("\t", bcolors.error("[ERROR fetching]"), un_remote, e)
+    print("\n", bcolors.bold("Total: {}/{}".format(num_fetched , len(repos))), "\n\n")
+
     
 def __get_dirty_repos__(*repos):
-    print(bcolors.HEADER + "\nRepositorios en estado DIRTY:\n" + bcolors.ENDC)
+    print("\n", bcolors.header("Repositorios en estado DIRTY:"), "\n")
     num_dirty=0
     for repo in repos:
         if not repo.bare and repo.is_dirty(untracked_files=True):
             print(bcolors.warning("[DIRTY REPO]"), repo.working_tree_dir)
             num_dirty += 1
-    print(bcolors.BOLD + "\nTotal: {}/{}".format(num_dirty , len(repos)), "\n\n" + bcolors.ENDC)
+    print("\n", bcolors.bold("Total: {}/{}".format(num_dirty , len(repos))), "\n\n")
 
 def __procress_parameters__():
     if len(sys.argv) <= 2: return {} 
@@ -192,17 +207,19 @@ def connect(**kwargs):
         for un_repo in repos(print=False):
             if not un_repo.bare:
                 remote_path = cadena_conexion + un_repo.working_tree_dir
-                remote = __create_remote__(un_repo, name, remote_path)
-                if remote: num_conected += 1
-        print(bcolors.BOLD + "\nConectados: {}".format(num_conected), "\n\n" + bcolors.ENDC)
+            elif un_repo.bare:
+                remote_path = cadena_conexion + un_repo.git_dir
+            remote = __create_remote__(un_repo, name, remote_path)
+            if remote: num_conected += 1
+        print("\n", bcolors.bold("Conectados: {}".format(num_conected)), "\n\n")
     else: 
         bcolors.error("Abortando")
         exit()
 
 
-def fetch(print=True, **kwargs):
-    name_remote = kwargs["--remote"] if "--remote" in kwargs else None
-    if print == True: __fetch_repos__(*repos(print=False), name_remote=name_remote)
+def fetch(**kwargs):
+    name_remote = kwargs["--name"] if "--name" in kwargs else None
+    __fetch_repos__(*repos(print=False), name_remote=name_remote)
 
 # Comandos posibles
 switch_cmd = {
